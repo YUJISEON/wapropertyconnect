436217bbea7b9d1c3f639defeaa2c2ea524d266e
YUJISEON <ryujiseon25@gmail.com>
2024-08-03 22:23:18 +0900

    update

--- agents/js/agents.js
+++ agents/js/agents.js
@@ -1,94 +1,213 @@
 window.addEventListener("DOMContentLoaded", function(){
 
-    async function fetchPostcodes(query) {
-        const response = await fetch(`https://api.zippopotam.us/AU/${query}`);
-        if (response.ok) {
-            const data = await response.json();
-            return data.places.map(place => `${place['place name']} ${place['state abbreviation']} ${query}`);
-        } else {
-            return [];
-        }
-    }
+    const page = document.getElementById('subPage');
     
-    const input = document.getElementById('postcode');
-    const suggestionsList = document.getElementById('suggestions');
-    const searchBtn = document.getElementById('searchBtn');
-    const contactForm = document.getElementById('contactForm');
-    // const step1 = document.getElementById('step1');
-    // const step2 = document.getElementById('step2');
-    // const step3 = document.getElementById('step3');
-    // const step4 = document.getElementById('step4');
-    
-    // const option1 = document.getElementById('option1');
-    // const option2 = document.getElementById('option2');
-    
-    // const options = document.querySelectorAll('.option');
-    // const propertyTypes = document.querySelectorAll('.property-type');
-    
-    input.addEventListener('input', async () => {
-        const query = input.value.toLowerCase();
-        suggestionsList.innerHTML = '';
-        if (query) {
-            const postcodes = await fetchPostcodes(query);
-            postcodes.forEach(postcode => {
-                const li = document.createElement('li');
-                li.textContent = postcode;
-                li.addEventListener('click', () => {
-                    input.value = postcode;
-                    suggestionsList.innerHTML = '';
-                });
-                suggestionsList.appendChild(li);
-            });
+    if ( page.classList.contains('agents') ) {
 
-            if( postcodes.length ) {
-                suggestionsList.classList.add('on');
+        async function fetchPostcodes(query) {
+            const response = await fetch(`https://api.zippopotam.us/AU/${query}`);
+            if (response.ok) {
+                const data = await response.json();
+                return data.places.map(place => `${place['place name']} ${place['state abbreviation']} ${query}`);
             } else {
-                suggestionsList.classList.remove('on');
+                return [];
             }
         }
-    });
-
-    suggestionsList.addEventListener('click', function(e) {
-        if (e.target && e.target.nodeName === 'LI') {
-            input.value = e.target.innerText;
+        
+        const input = document.getElementById('postcode');
+        const suggestionsList = document.getElementById('suggestions');
+        const searchBtn = document.getElementById('searchBtn');
+        
+        input.addEventListener('input', async () => {
+            const query = input.value.toLowerCase();
             suggestionsList.innerHTML = '';
-        }
-    });
+            if (query) {
+                const postcodes = await fetchPostcodes(query);
+                postcodes.forEach(postcode => {
+                    const li = document.createElement('li');
+                    li.textContent = postcode;
+                    li.addEventListener('click', () => {
+                        input.value = postcode;
+                        suggestionsList.innerHTML = '';
+                    });
+                    suggestionsList.appendChild(li);
+                });
 
-    let postValue = '';
+                if( postcodes.length ) {
+                    suggestionsList.classList.add('on');
+                } else {
+                    suggestionsList.classList.remove('on');
+                }
+            }
+        });
 
-    searchBtn.addEventListener('click', function(e) {
-        if (!input.value) {
-            alert('Enter your postcode..');
-            return false;
-        } 
+        suggestionsList.addEventListener('click', function(e) {
+            if (e.target && e.target.nodeName === 'LI') {
+                input.value = e.target.innerText;
+                suggestionsList.innerHTML = '';
+            }
+        });
 
-        postValue = input.value;
-        location.replace('/wapropertyconnect/agents/finder.html');
-    });
+        let postValue = '';
 
+        searchBtn.addEventListener('click', function(e) {
+            if (!input.value) {
+                alert('Enter your postcode..');
+                return false;
+            } 
 
-    // contactForm.addEventListener('submit', function(event) {
-    //     event.preventDefault(); // prevent reload
+            postValue = input.value;
+            location.replace('/wapropertyconnect/agents/finder.html');
+        });
+
+    } else {
+
+        const contactForm = document.getElementById('contactForm');
+        const progress = document.getElementById('progress');
+        const step1 = document.getElementById('step1');
+        const step2_01 = document.getElementById('step2-01');
+        const step2_02 = document.getElementById('step2-02');
+        const step2_03 = document.getElementById('step2-03');
+        const step3 = document.getElementById('step3');
+        const step4 = document.getElementById('step4');
+        const step5_01 = document.getElementById('step5-01');
+        const step5_02 = document.getElementById('step5-02');
+        const section1 = document.getElementById('section1');
+        const section2 = document.getElementById('section2');
     
-    //     var formData = new FormData(this);
-    //     formData.append('service_id', 'service_berj6c8');
-    //     formData.append('template_id', 'template_v42hb05');
-    //     formData.append('user_id', 'nmWuFqNcSu-nC5H9Z');
+        const option1 = document.getElementById('step1-option');
+        const option2_01 = document.getElementById('step2-01-option');
+        const option2_02 = document.getElementById('step2-02-option');
+        const option2_03 = document.getElementById('step2-03-option');
+        const option3 = document.getElementById('step3-option');
+        const option4 = document.getElementById('step4-option');
+        const option5_01 = document.getElementById('step5-01-option');
     
-    //     var xhr = new XMLHttpRequest();
-    //     xhr.open('POST', 'https://api.emailjs.com/api/v1.0/email/send-form', true);
-    //     xhr.onload = function() {
-    //         if (xhr.status >= 200 && xhr.status < 300) {
-    //             alert('Your mail is sent!');
-    //         } else {
-    //             alert('Oops... ' + xhr.responseText);
-    //         }
-    //     };
-    //     xhr.onerror = function() {
-    //         alert('Oops... Something went wrong.');
-    //     };
-    //     xhr.send(formData);
-    // });
+        const step1Options = step1.querySelectorAll('.col-inner');
+        const step2Options_01 = step2_01.querySelectorAll('.col-inner');
+        const step2Options_02 = step2_02.querySelectorAll('.col-inner');
+        const step2Options_03 = step2_03.querySelectorAll('.col-inner');
+        const step3Options = step3.querySelectorAll('.col-inner');
+        const step4Options = step4.querySelectorAll('.col-inner');
+        const step5Options_01 = step5_01.querySelectorAll('.col-inner');
+        const progressStep = progress.querySelectorAll('li');
+    
+        step1Options.forEach(option => {
+            option.addEventListener('click', () => {
+                option1.value = option.getAttribute('data-step1');
+    
+                step1.classList.remove('active');
+                
+                if( option1.value === 'Buy') {
+                    step2_01.classList.add('active');
+                } else {
+                    step2_02.classList.add('active');
+                }
+    
+                progressStep[1].classList.add('on');
+            });
+        });
+    
+        step2Options_01.forEach(option => {
+            option.addEventListener('click', () => {
+                option2_01.value = option.getAttribute('data-step2-01');
+    
+                step2_01.classList.remove('active');
+                step2_02.classList.add('active');
+
+                if( option1.value === 'Buy') {
+                    progressStep[2].classList.add('on');
+                }
+                progressStep[1].classList.add('on');
+            });
+        });
+    
+        step2Options_02.forEach(option => {
+            option.addEventListener('click', () => {
+                option2_02.value = option.getAttribute('data-step2-02');
+    
+                step2_02.classList.remove('active');
+                if( option1.value === 'Rent') {
+                    step2_03.classList.add('active');
+                } else {
+                    step3.classList.add('active');
+                    progressStep[2].classList.add('on');
+                }
+            });
+        });
+    
+        step2Options_03.forEach(option => {
+            option.addEventListener('click', () => {
+                option2_03.value = option.getAttribute('data-step2-03');
+    
+                step2_03.classList.remove('active');
+                step5_02.classList.add('active');
+
+                progressStep[2].classList.add('on');
+                progressStep[3].classList.add('on');
+                progressStep[4].classList.add('on');
+            });
+        });
+        
+    
+        step3Options.forEach(option => {
+            option.addEventListener('click', () => {
+                option3.value = option.getAttribute('data-step3');
+    
+                step3.classList.remove('active');
+                step4.classList.add('active');
+                progressStep[3].classList.add('on');
+            });
+        });
+    
+        step4Options.forEach(option => {
+            option.addEventListener('click', () => {
+                option4.value = option.getAttribute('data-step4');
+    
+                step4.classList.remove('active');
+                step5_01.classList.add('active');
+                progressStep[4].classList.add('on');
+            });
+        });
+    
+        step5Options_01.forEach(option => {
+            option.addEventListener('click', () => {
+                option5_01.value = option.getAttribute('data-step5-01');
+
+                console.log(option.getAttribute('data-step5-01'));
+                console.log(option5_01.value);
+    
+                step5_01.classList.remove('active');
+                section1.classList.remove('active');
+                section2.classList.add('active');
+                progressStep[5].classList.add('on');
+            });
+        });
+    
+        contactForm.addEventListener('submit', function(event) {
+            event.preventDefault(); // prevent reload
+        
+            var formData = new FormData(this);
+            formData.append('service_id', 'service_berj6c8');
+            formData.append('template_id', 'template_v42hb05');
+            formData.append('user_id', 'nmWuFqNcSu-nC5H9Z');
+        
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'https://api.emailjs.com/api/v1.0/email/send-form', true);
+            xhr.onload = function() {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    alert('Your mail is sent!');
+                } else {
+                    alert('Oops... ' + xhr.responseText);
+                }
+            };
+            xhr.onerror = function() {
+                alert('Oops... Something went wrong.');
+            };
+            xhr.send(formData);
+        });
+    }
+
+
     
 });

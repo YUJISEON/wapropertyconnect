29577e241e447dc913513ef1b42fa120a2dc0684
YUJISEON <ryujiseon25@gmail.com>
2024-08-04 21:53:25 +0900

    update

--- calculator/js/calculator.js
+++ calculator/js/calculator.js
@@ -1,31 +1,33 @@
 window.addEventListener("DOMContentLoaded", function(){ 
 
-    const sliderRange = document.getElementById("rangeSlider");
+    const rangeSliderWrap = document.getElementById("rangeSliderWrap");
+    const rangeSlider = document.getElementById("rangeSlider");
     const handle = document.getElementById("handle");
     const basicPrice = document.querySelector(".basic-price");
-    
+    const comm = document.getElementById("comm");
+    const commPrice = document.getElementById("commPrice");
+    const resultPrice = document.getElementById("resultPrice")
     const maxRange = 6000000;
 
-    function updateSlider() {
-        console.log('updateSlider');
-
-        const handlePosition = gsap.getProperty(handle, "x");
-        const rangeWidth = sliderRange.offsetWidth;
-        const percentage = handlePosition / rangeWidth;
-        const value = Math.round(percentage * maxRange);
-
-        sliderRange.style.background = `linear-gradient(to right, #333 ${percentage * 100}%, #ddd ${percentage * 100}%)`;
-        basicPrice.textContent = value.toLocaleString();
+    function formatNumberWithCommas(number) {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
 
-    Draggable.create(handle, {
-        type: "x",
-        bounds: sliderRange,
-        onDrag: updateSlider,
-        onDragEnd: updateSlider
-    });
+    function updateSlider() {
+        let xPercent = gsap.utils.mapRange(0, slider[0].maxX, 0, 100),
+            changePrice = (maxRange * xPercent(slider[0].x)) * 0.01,
+            commValue = parseFloat(comm.value) / 100;
 
-    // 초기값 설정
-    updateSlider();
+        rangeSlider.style.width = xPercent(slider[0].x) + '%';
+        basicPrice.innerText = formatNumberWithCommas(Math.round(changePrice));
+        commPrice.innerText = formatNumberWithCommas(Math.round(commValue*changePrice));
+        resultPrice.innerText = formatNumberWithCommas(Math.round(changePrice-(commValue*changePrice)));
+    }
+
+    const slider = Draggable.create(handle, {
+        type: "x",
+        bounds: rangeSliderWrap,
+        onDrag: updateSlider,
+    });
 
 });

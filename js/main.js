f1fd31b948f5ce16ff41bc245ec407fe1ac9fa2a
YUJISEON <ryujiseon25@gmail.com>
2024-08-17 15:49:15 +0900

    update

--- js/main.js
+++ js/main.js
@@ -19,14 +19,28 @@ window.addEventListener('DOMContentLoaded', function(){
 
     ///////////////////////////////////////////////////
 
-    var location = { lat: -32.03991, lng: 115.88508 };
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: location,
-        zoom: 18
-    });
+    function initMap() {
+        var location = { lat: -32.0405267, lng: 115.8850593 };
+        
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 18,
+            center: location
+        });
+        
+        // 마커 생성
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+        });
+        
+        // InfoWindow 생성 및 텍스트 설정
+        var infowindow = new google.maps.InfoWindow({
+            content: '<h3>WA Property Connect</h3>'
+        });
+        
+        // 처음부터 InfoWindow를 열기
+        infowindow.open(map, marker);
+    }
 
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map
-    });
+    initMap();
 })

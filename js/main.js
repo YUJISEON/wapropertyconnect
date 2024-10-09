a36064b77ee3a5331c3d5b0e54d16ec4b490c0e2
YUJISEON <ryujiseon25@gmail.com>
2024-10-09 20:01:27 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,29 +1,7 @@
 
 window.addEventListener('DOMContentLoaded', function(){
 
-    var agentSwiperer = new Swiper(".agentSwiper", {
-        slidesPerView: 2,
-        spaceBetween: 10,
-        loop: true,
-        navigation: {
-            nextEl: ".agentSwiper + .swiper-button-next",
-            prevEl: ".agentSwiper + .swiper-button-prev",
-        },
-        breakpoints: {
-            481: {
-                slidesPerView: 3,
-                spaceBetween: 10,
-            },
-            769: {
-                slidesPerView: 4,
-                spaceBetween: 20,
-            },
-            961: {
-                slidesPerView: 5,
-                spaceBetween: 20,
-            },
-        },
-    });
+
 
 
     ///////////////////////////////////////////////////

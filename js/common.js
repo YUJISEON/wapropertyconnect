46901e06313806268551cd6d73bd534a0ee2cc64
YUJISEON <ryujiseon25@gmail.com>
2024-10-13 19:34:14 +0900

    update

--- js/common.js
+++ js/common.js
@@ -159,26 +159,30 @@ window.addEventListener('DOMContentLoaded', function(){
     gsap.registerPlugin(ScrollTrigger);
 
     const header = document.querySelector('#header');
-    const showHeader = gsap.from(header, { 
-        top: `-=${header.offsetHeight}`,
-        paused: true,
-        duration: .3,
-        ease: 'power3.easeOut'
-    }).progress(1);
+    const finder = document.querySelector('.finder');
 
-    ScrollTrigger.create({
-		start: 'top top',
-		end: 'max',
-		onUpdate: (self) => {  
-            if (self.direction === -1) { 
-                showHeader.play();
-                header.classList.add('fixed');
-            } else { 
-                showHeader.reverse();
-                header.classList.remove('fixed');
+    if( !finder) {
+        const showHeader = gsap.from(header, { 
+            top: `-=${header.offsetHeight}`,
+            paused: true,
+            duration: .3,
+            ease: 'power3.easeOut'
+        }).progress(1);
+    
+        ScrollTrigger.create({
+            start: 'top top',
+            end: 'max',
+            onUpdate: (self) => {  
+                if (self.direction === -1) { 
+                    showHeader.play();
+                    header.classList.add('fixed');
+                } else { 
+                    showHeader.reverse();
+                    header.classList.remove('fixed');
+                }
             }
-		}
-	});
+        });
+    }
 
     const parallaxArea = document.querySelector('.parallax-area')
     const parallaxBg = document.querySelector('.parallax-bg')

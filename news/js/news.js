ba4a9acf9f87293ffa41d5bda6381fd3e2bf5e0e
YUJISEON <ryujiseon25@gmail.com>
2024-10-13 20:03:19 +0900

    update

--- news/js/news.js
+++ news/js/news.js
@@ -2,7 +2,17 @@ window.addEventListener("DOMContentLoaded", function(){
     var guideSwiper = new Swiper(".guideSwiper", {
         slidesPerView: 1,
         spaceBetween: 0,
-        //loop: true,
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        coverflowEffect: {
+            rotate: 0,
+            depth: 150,
+            stretch: 0,
+            modifier: 1,
+            slideShadows: false
+        },
+        loop: true,
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
@@ -11,14 +21,23 @@ window.addEventListener("DOMContentLoaded", function(){
             481: {
                 slidesPerView: 2,
                 spaceBetween: 10,
+                coverflowEffect: {
+                    stretch: -20,
+                },
             },
             768: {
                 slidesPerView: 2,
                 spaceBetween: 15,
+                coverflowEffect: {
+                    stretch: -50,
+                },
             },
             961: {
                 slidesPerView: 3,
                 spaceBetween: 15,
+                coverflowEffect: {
+                    stretch: -70,
+                },
             },
         },
     });
